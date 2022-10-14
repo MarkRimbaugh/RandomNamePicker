54c2523f43b0da001d35cffc8ec4744a433665dc
Mark Rimbaugh <mark.rimbaugh@outlook.com>
2022-10-14 17:28:15 -0400

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,44 @@
-﻿List<string> nameList = new List<string>();
-
-nameList.Add("Alex");
-nameList.Add("Brian");
-nameList.Add("David");
-nameList.Add("Donovan");
-nameList.Add("Drew");
-nameList.Add("Erik");
-nameList.Add("Greg");
-nameList.Add("Jackson");
-nameList.Add("Joshua");
-nameList.Add("Mark");
-nameList.Add("Mursal");
-nameList.Add("Patrick");
-nameList.Add("Rico");
-nameList.Add("Rohin");
-
-foreach (var name in nameList)
+﻿public class Program
 {
-    System.Console.WriteLine(name);
+    private static void Main(string[] args)
+    {
+        List<string> names = new List<string>(){"Alex", "Brian", "David", "Donovan", "Drew", "Erik", "Greg",
+        "Jackson", "Joshua", "Mark", "Mursal", "Patrick", "Rico", "Rohin"};
+
+        int initialNameListCount = names.Count;
+
+        Console.WriteLine("List size: " + names.Count);
+        Random rnd = new Random();
+
+        Stack<string> nameList = new Stack<string>();
+
+        for (int i = 0; i < initialNameListCount; i++)
+        {
+            int a = rnd.Next(0, names.Count);
+            nameList.Push(names[a]);
+            names.RemoveAt(a);
+        }
+        
+        bool keepGoing = true;
+        while (nameList.Count > 0 && keepGoing)
+        {
+            Console.WriteLine("Enter 'next' for a new name, or 'exit' to quit.");
+            string input = Console.ReadLine();
+            if(input == "next")
+            {
+                Console.WriteLine($"Name: {nameList.Peek()} -  {nameList.Count -1} names remain.");
+                nameList.Pop();
+            }
+
+            else if(input == "exit") 
+            {
+                keepGoing = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry, please try again.");
+            }
+        }
+        Console.WriteLine("Done looping. Goodbye!");
+    }
 }
-
-
-
