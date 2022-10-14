b2a731ba92cc6d0ea3df436e93ed4689adf3f52d
Mark Rimbaugh <mark.rimbaugh@outlook.com>
2022-10-14 17:27:22 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> nameList = new List<string>();
+
+nameList.Add("Alex");
+nameList.Add("Brian");
+nameList.Add("David");
+nameList.Add("Donovan");
+nameList.Add("Drew");
+nameList.Add("Erik");
+nameList.Add("Greg");
+nameList.Add("Jackson");
+nameList.Add("Joshua");
+nameList.Add("Mark");
+nameList.Add("Mursal");
+nameList.Add("Patrick");
+nameList.Add("Rico");
+nameList.Add("Rohin");
+
+foreach (var name in nameList)
+{
+    System.Console.WriteLine(name);
+}
+
+
+
